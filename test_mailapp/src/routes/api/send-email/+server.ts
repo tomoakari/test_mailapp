91ae915f7c0c780d1636014b3af7e305d154ad77
tomoyuki matsumoto <matsumoto.tomoyuki@valuesccg.com>
2025-02-04 17:17:54 +0900

    修正

---
+++ test_mailapp/src/routes/api/send-email/+server.ts
@@ -0,0 +1,41 @@
+import { json } from '@sveltejs/kit'; 
+import Mailjet from 'node-mailjet'
+
+const MAILJET_API_KEY = "5da0559dbcf225700d9ed51dcb8fc1c3"
+const MAILJET_API_SECRET = "7bafeb401ad08c3abe410c4957e092a5"
+const SENDER_EMAIL = "info@openfire.site"
+
+
+const mailjet = new Mailjet({
+  apiKey: MAILJET_API_KEY,
+  apiSecret: MAILJET_API_SECRET
+})
+
+export async function POST({ request }) {
+  const { email, content } = await request.json();
+
+  try {
+    await mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: SENDER_EMAIL,
+            Name: "メール送信アプリ"
+          },
+          To: [
+            {
+              Email: email,
+              Name: email
+            }
+          ],
+          Subject: "新着メッセージ",
+          TextPart: content
+        }
+      ]
+    });
+
+    return json({ success: true });
+  } catch (error) {
+    return json({ success: false, error: error.message }, { status: 500 });
+  }
+}
